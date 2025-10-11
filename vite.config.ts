2002a8f865446cd76a48f8963f32bfb431131a99
Sintija <sintija649@gmail.com>
2025-10-11 11:46:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/se-investing/" : "/se-investing/",
+  base: mode === "development" ? "" : "/",
   server: {
     host: "::",
     port: 8081,
